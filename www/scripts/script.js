a3e26a5a211b2f30d6b55474ade5caa29eb63b1e
Aysanat <aysanat21aysanat@gmail.com>
2024-12-05 13:04:21 +0300

    Скролл до формы, изменение цвета при клике

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,22 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  $('.js-click').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-click').removeClass('click');
+    $(this).addClass('click');
+
+  });
+
+  $('.js-btn-scroll').on('click', function() {
+
+    $('html, body').animate({
+      scrollTop: $('#order').offset().top
+    }, {
+      duration: 370,
+      easing: 'linear'
+    });
+
+    return;
+  });
+})
